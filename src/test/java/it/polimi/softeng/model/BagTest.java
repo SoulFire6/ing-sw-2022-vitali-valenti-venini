7e56623d50800576c2acbbe7aea31f955912f9f3
Callum-Venini <callum.venini@mail.polimi.it>
2022-04-02 17:21:13 +0200

    test: updated tests for Bag_Tile class

--- src/test/java/it/polimi/softeng/model/BagTest.java
+++ src/test/java/it/polimi/softeng/model/BagTest.java
@@ -4,8 +4,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.util.EnumMap;
+
 public class BagTest {
-    //TODO finish tests
     @Test
     public void testBagConstructor() {
         Integer initValue=0;
@@ -13,6 +14,30 @@ public class BagTest {
         for (Colour c: Colour.values()) {
             assertEquals("Bag was not initialised correctly: "+c,initValue,b.getContents().get(c));
         }
+    }
+    @Test
+    public void testDrawStudents() {
+        EnumMap<Colour,Integer> testDraw;
+        Integer testDrawNum=5;
+        Integer testSum=0;
+        Bag_Tile b=new Bag_Tile(testDrawNum);
+        testDraw=b.drawStudents(testDrawNum);
+        for (Colour c: Colour.values()) {
+            testSum+=testDraw.get(c);
+        }
+        assertEquals(testSum,testDrawNum);
+    }
+    @Test
+    public void testDrawStudentsWithLessThanRequired() {
+        EnumMap<Colour,Integer> testDraw;
+        Integer testDrawNum=10;
+        Integer testSum=0;
+        Bag_Tile b=new Bag_Tile(1);
+        testDraw=b.drawStudents(testDrawNum);
+        for (Colour c: Colour.values()) {
+            testSum+=testDraw.get(c);
+        }
+        assertTrue(testSum<testDrawNum);
     }
 
 
