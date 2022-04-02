65cb372c8c75d67bfb75655df9cfc0b1de83eaa7
Callum-Venini <callum.venini@mail.polimi.it>
2022-04-02 17:04:43 +0200

    test: updated tests for CharacterCardTest class

--- src/test/java/it/polimi/softeng/model/CharacterCardTest.java
+++ src/test/java/it/polimi/softeng/model/CharacterCardTest.java
@@ -1,14 +1,29 @@
 package it.polimi.softeng.model;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 
 public class CharacterCardTest {
-    //TODO finish tests
+    @Test
+    public void testCharacterCardConstructor() {
+        String testID="test";
+        Integer testCost=0;
+        CharacterCard testCard=new CharacterCard(testID,testCost);
+        Assert.assertEquals(testCard.getCardID(),testID);
+        Assert.assertEquals(testCard.getCost(),testCost);
+    }
+    @Test
+    public void testIncrementCost() {
+        Integer testValue=0;
+        CharacterCard testCard=new CharacterCard("test",testValue);
+        testCard.incrementCost();
+        testValue+=1;
+        assertEquals(testCard.getCost(),testValue);
+    }
     @Test
     public void testGenCharacterCards() {
         Integer num=3;
