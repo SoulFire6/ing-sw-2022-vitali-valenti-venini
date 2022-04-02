758af5ee226794cedcc6f2e812bec8c3d1d24a80
Callum-Venini <callum.venini@mail.polimi.it>
2022-04-02 22:29:00 +0200

    test: added tests for class Island_Tile

---
+++ src/test/java/it/polimi/softeng/model/IslandTest.java
@@ -0,0 +1,68 @@
+package it.polimi.softeng.model;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class IslandTest {
+    @Test
+    public void testIslandConstructor() {
+        String testID="test";
+        Island_Tile island=new Island_Tile(testID);
+        assertEquals(testID,island.getTileID());
+        assertFalse(island.getMotherNature());
+        assertEquals(0,island.getTowers().intValue());
+        assertNull(island.getTeam());
+        assertNull(island.getNext());
+        assertNull(island.getPrev());
+        assertFalse(island.getNoEntry());
+    }
+    @Test
+    public void testMotherNature() {
+        Island_Tile island=new Island_Tile("MotherNatureTest");
+        island.setMotherNature(true);
+        assertEquals(true,island.getMotherNature());
+    }
+    @Test
+    public void testTower() {
+        Integer num=8;
+        Island_Tile island=new Island_Tile("TowerTest");
+        island.setTowers(num);
+        assertEquals(num,island.getTowers());
+    }
+    @Test
+    public void testTeam() {
+        Island_Tile island=new Island_Tile("TeamTest");
+        assertNull(island.getTeam());
+        for (Team team: Team.values()) {
+            island.setTeam(team);
+            assertEquals(team,island.getTeam());
+        }
+    }
+    @Test
+    public void testPointers() {
+        String islandOneID="1";
+        String islandTwoID="2";
+        Island_Tile one=new Island_Tile(islandOneID);
+        Island_Tile two=new Island_Tile(islandTwoID);
+        one.setNext(two);
+        assertEquals(two,one.getNext());
+        one.getNext().setPrev(one);
+        assertNotEquals(one,one.getNext());
+        assertEquals(one,one.getNext().getPrev());
+    }
+    @Test
+    public void testNoEntry() {
+        Island_Tile island=new Island_Tile("NoEntryTest");
+        island.setNoEntry(true);
+        assertTrue(island.getNoEntry());
+    }
+    @Test
+    public void testGenIslands() {
+        Integer num=12;
+        ArrayList<Island_Tile> islands=Island_Tile.genIslands(num);
+        assertEquals(num.intValue(),islands.size());
+    }
+}
