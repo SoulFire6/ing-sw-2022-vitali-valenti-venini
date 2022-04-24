81f6fb3c011333ec94f3adffff0db51160179dfe
Callum-Venini <callum.venini@mail.polimi.it>
2022-04-24 16:55:15 +0200

    test: updated tests for CharacterCard

--- src/test/java/it/polimi/softeng/model/CharacterCardTest.java
+++ src/test/java/it/polimi/softeng/model/CharacterCardTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class CharacterCardTest {
     @Test
@@ -23,5 +23,22 @@ public class CharacterCardTest {
         testCard.incrementCost();
         testValue+=1;
         assertEquals(testCard.getCost(),testValue);
+    }
+    @Test
+    public void testMemory() {
+        CharacterCard testCardNoMem=new CharacterCard("test_noMem",0);
+        assertNull(testCardNoMem.getMemory());
+        Object obj=new Object();
+        //Testing example anonymous class, like the ones in controller
+        CharacterCard testCardMem=new CharacterCard("test_mem",0,obj) {
+            @Override
+            public Object getMemory() {
+                return this.memory;
+            }
+        };
+        assertEquals(testCardMem.getMemory(),obj);
+        Integer testInt=0;
+        testCardMem.setMemory(testInt);
+        assertEquals(testCardMem.getMemory(),testInt);
     }
 }
